c9b423742d1dba6eb99b7929207778469fa6f0f4
Rudra Goel <goelrudra2016@gmail.com>
2025-07-28 23:04:30 -0400

    Initial Commit

---
+++ ClipboardMenuBar/ClipboardMenuBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipboardMenuBarApp.swift
+//  ClipboardMenuBar
+//
+//  Created by Rudra Goel on 7/28/25.
+//
+
+import SwiftUI
+
+@main
+struct ClipboardMenuBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
